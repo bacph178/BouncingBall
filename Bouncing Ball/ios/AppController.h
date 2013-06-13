daf40a6f85acbe948009aaaa2a6f64902d80393a
macbook_016 <Admin@macbook-016.local>
2013-06-13 13:34:06 +0700

    Initial Commit

---
+++ Bouncing Ball/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Bouncing_BallAppController.h
+//  Bouncing Ball
+//
+//  Created by macbook_016 on 2013/06/13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Bouncing Ball/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Bouncing_BallAppController.h
+//  Bouncing Ball
+//
+//  Created by macbook_016 on 2013/06/13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
